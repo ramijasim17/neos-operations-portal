c1852f077cc414c9a9e279a1872b5f1e9bf3e3a1
ramijasim17 <ramijasim17@gmail.com>
2026-09-09 00:44:05 +0300

    Update urls.py

--- neos_portal 2/neos_portal/urls.py
+++ neos_portal 2/neos_portal/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("equipment/", include("equipment.urls")),
+    path("maintenance/", include("maintenance.urls")),
     path("", include("core.urls")),
 ]
 
